d8286f7056f7962f69fbe6b8a4ae3b50eb3903af
Nishchay Kaushik <nishchay@hackerrank.com>
2020-02-15 18:50:40 +0530

    Create Splash Activity

---
+++ src/com/shealth2fit/SplashActivity.java
@@ -0,0 +1,21 @@
+package com.shealth2fit;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_splash);
+    new Handler().postDelayed(() -> {
+      Intent i = new Intent(getApplicationContext(), MainActivity.class);
+      startActivity(i);
+      finish();
+    }, 2000);
+  }
+}
